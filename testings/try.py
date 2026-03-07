a4c75883fafad2fdb1d4ff85c0c78fb052c6cbcb
G-Abhishek-Srivatsasa <srivatsasa.edu@gmail.com>
2026-03-07 23:08:29 +0530

    Delete try.py

--- testings/try.py
+++
@@ -1,8 +0,0 @@
-from siftout import Janitor 
-
-cleaner = Janitor(extra_patterns=["*.bak"])
-report = cleaner.self_destruct()
-print(f"Cleaner Report: {report}") 
-
-cleaner.secure_env()
-print("Environment Secured")
